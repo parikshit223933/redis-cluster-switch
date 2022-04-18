ba851de21e9b2cbb3d9d6c22384a9ef76adf9a7b
Parikshit Singh <pk223933@gmail.com>
2022-04-18 21:00:50 +0530

    Redis cluster switch

---
+++ README.md
@@ -0,0 +1,3 @@
+# Redis cluster switch scripts
+
+This repo is WIP. I am creating short scripts to switch between clustered and non clustered modes quickly in redis.
---
+++ redis-6.2.x/scripts.sh
@@ -0,0 +1,29 @@
+# stop all running cluster nodes
+./create-cluster stop
+
+# Clean cluster node conf and log files 
+./create-cluster clean
+
+# Start cluster nodes
+./create-cluster start
+
+# start redis server with provided configuration
+redis-server ./redis.conf
+
+# create cluster
+redis-cli --cluster create 127.0.0.1:6379 127.0.0.1:6380 127.0.0.1:6381 127.0.0.1:6382 127.0.0.1:6383 127.0.0.1:6384 127.0.0.1:6385 --cluster-replicas 1
+
+# Connect to redis client with given host and port
+redis-cli -h 127.0.0.1 -p 6379
+
+# Get all redis cluster nodes
+redis-cli -h 127.0.0.1 -p 6379 cluster nodes
+
+# Flush all keys in redis cluster
+redis-cli flushall
+
+# Reset redis cluster
+redis-cli cluster reset
+
+# Make a call for all nodes
+redis-cli --cluster 127.0.0.1:6379 call keys "*"
